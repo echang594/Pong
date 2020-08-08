b71ae71b3425b0a45075c6c1e9bd92575a74cb67
kren29 <66499147+kren29@users.noreply.github.com>
2020-08-08 11:25:28 -0700

    Update Pong.java

--- src/Pong.java
+++ src/Pong.java
@@ -1,9 +1,29 @@
+import java.awt.*;
+import javax.swing.*;
 
 public class Pong {
 
+	public static final int gameWidth = 1000;
+	public static final int gameHeight = 600;
+	
+	private JFrame frame;
+	private JPanel panel; 
+	
+	public Pong() {
+		
+		frame = new JFrame();
+		frame.setBounds(50, 50, gameWidth, gameHeight);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Pong");
+	
+		frame.setResizable(false);
+		frame.setVisible(true);
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		new Pong();
 	}
 
 }
