a391be9c20b62002180a0001770b1cc5cf791d16
AndyDing28 <dingandy28@gmail.com>
2020-08-08 12:43:14 -0700

    Create Ball.java
    Ball

---
+++ src/Ball.java
@@ -0,0 +1,38 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball {
+
+	int x, y, radius;
+	
+	public Ball(int x, int y, int radius) {
+	
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+	 
+	public void draw(Graphics g) {
+		
+		g.setColor(Color.WHITE);
+		g.fillOval(x,  y,  radius, radius);
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public int getRadius() {
+		return x;
+	}
+	
+	public boolean isInContact() {
+		
+		return false;
+	}
+}
