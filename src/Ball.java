4f4ad1d6dad780a14b8da62aa31736906301a241
AndyDing28 <dingandy28@gmail.com>
2020-08-08 17:29:52 -0700

    um fixed scores

--- src/Ball.java
+++ src/Ball.java
@@ -41,9 +41,17 @@ public class Ball {
 	public int getY() {
 		return y;
 	}
-
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 	public int getRadius() {
-		return x;
+		return radius;
 	}
 
 	public boolean isInContact() {
